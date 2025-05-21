5457de73c46d02e7e3145a18abdc5b2c58ae1f9b
Pedro Victor Abreu <pedro.fabreu97@gmail.com>
2025-05-21 00:21:03 -0300

    update

--- lib/api_constants.dart
+++ lib/api_constants.dart
@@ -1,1 +1,2 @@
-const String apiBaseUrl = 'http://localhost:8000/api/v1';
+const String apiBaseUrl = 'https://stfiapi.pedrovfabreu.com.br/api/v1';
+const String apiBaseUrlLocal = 'http://localhost:8000/api/v1';
